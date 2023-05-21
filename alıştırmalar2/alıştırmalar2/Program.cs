921c83c6e8987927d12f4b1b295f14e9e13349a4
Sabri Alperen Kılıç <75065926+EOLO03@users.noreply.github.com>
2023-05-21 18:20:29 +0300

    C# console toplama uygulaması
    C# ilk öğrenmeye başladığım zamanlar yazdığım 2 sayı arasında basit toplama işlemi yapan console uygulaması.

---
+++ alıştırmalar2/alıştırmalar2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alıştırmalar2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayı_1, sayı_2, toplam = 0;
+            Console.Write("sayı_1 değeri:");
+            sayı_1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("sayı_2 değeri:");
+            sayı_2 = Convert.ToInt32(Console.ReadLine());
+            toplam = sayı_1 + sayı_2;
+            Console.Write("toplam=" + toplam);
+            Console.ReadLine();
+        }// BU CONSOLE PROGRAMI TOPLAMA İŞLEMİ İÇİN YAZILMIŞTIR. ÖĞRENME AŞAMASINDAYIM :)
+    }
+}
